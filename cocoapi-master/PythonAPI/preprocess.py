c08895c2a16a82eb21bc791c3562a7cf6298fa42
AnanyaJ <ananyaj74@gmail.com>
2018-07-27 14:52:32 -0700

    Reorganized code.

--- cocoapi-master/PythonAPI/preprocess.py
+++ cocoapi-master/PythonAPI/preprocess.py
@@ -1,11 +1,14 @@
 from pycocotools.coco import COCO
-import io
-import requests
-from PIL import Image
+import cv2
 from torchvision import transforms
 import torch
 from nltk import word_tokenize
-import pickle
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+dtype = torch.float
 
 def is_number(s):
     """Returns True if the string s is any kind of number (includes decimals)."""
@@ -26,44 +29,68 @@ def tokenize(caption):
             tokens[i] = '<num>' # replaces all numbers with number token
     return tokens
 
-dataDir = '..'
-dataType = 'train2014'
-annFile = '%s/annotations/instances_%s.json' % (dataDir, dataType)
-capFile = '%s/annotations/captions_%s.json' % (dataDir, dataType)
-caps = COCO(capFile)
+def get_images(dataDir, dataType):
+    """
+    Returns information for all MS COCO images from the dataType (e.g. 'train2014') dataset.
+    """
+    annFile = '%s/annotations/instances_%s.json' % (dataDir, dataType)
+    coco = COCO(annFile)
+    imgIds = coco.getImgIds()
+    return coco.loadImgs(imgIds)
 
-# retrieve images
-coco = COCO(annFile)
-imgIds = coco.getImgIds()
-images = coco.loadImgs(imgIds)
+def get_image_data(dataDir, dataType, images):
+    """
+    Returns tensor filled with the data from images, which are first resized and cropped.
+    """
+    process = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor()
+    ])
 
-print('%d Images Loaded.' % len(images))
+    imgData = torch.zeros(len(images), 3, 224, 224, device=device, dtype=dtype)
 
-process = transforms.Compose([
-    transforms.Resize(224),
-    transforms.CenterCrop(224),
-    transforms.ToTensor()
-])
+    for i in range(len(images)):
+        imgData[i] = process(cv2.imread('%s/datasets/mscoco/%s/%s' % (dataDir, dataType, images[i]['file_name']))) # resize image and convert to tensor
 
-img_data = torch.zeros(len(images), 3, 224, 224, dtype=torch.float)
-captions = []
+    return imgData
 
-for i in range(len(images)):
-    img_url = images[i]['flickr_url']
-    img_data[i] = process(Image.open(io.BytesIO(requests.get(img_url).content))) # resize image and convert to tensor
+def get_captions(dataDir, dataType, images):
+    """
+    Returns list of tokenized captions for each image in images.
+    """
+    capFile = '%s/annotations/captions_%s.json' % (dataDir, dataType)
+    caps = COCO(capFile)
 
-    annIds = caps.getAnnIds(imgIds=images[i]['id']) # retrieve captions for image
-    anns = caps.loadAnns(annIds)
-    imageCaps = []
-    for ann in anns:
-        imageCaps.append(tokenize(ann['caption']))
-    captions.append(imageCaps)
+    captions = []
 
-# store image data
-torch.save(img_data, 'img_data.pt')
+    for i in range(len(images)):
+        annIds = caps.getAnnIds(imgIds=images[i]['id'])  # retrieve captions for image
+        anns = caps.loadAnns(annIds)
+        imageCaps = []
+        for ann in anns:
+            imageCaps.append(tokenize(ann['caption']))
+        captions.append(imageCaps)
 
-# store captions
-save_captions = open('captions.pickle', 'wb')
-pickle.dump(captions, save_captions)
-save_captions.close()
+    return captions
 
+def create_encoding(captions):
+    """
+    Maps each word present in captions to an integer. Returns the vocabulary list and this mapping.
+    """
+    wordCounts = {}
+    for imgCaps in captions:
+        for cap in imgCaps:
+            for word in cap:
+                if word in wordCounts:
+                    wordCounts[word] += 1
+                else:
+                    wordCounts[word] = 1
+
+    vocab = list(wordCounts.keys())
+    vocabEncoding = {}  # map each word to an integer
+    for i in range(len(vocab)):
+        vocabEncoding[vocab[i]] = i
+
+    return (vocab, vocabEncoding)
